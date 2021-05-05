51421a7de905336d06699aa5136509c681243cf2
tmampa <tmampa94@gmail.com>
2021-05-05 11:43:04 +0200

    Add my_each_with_index

--- enums.rb
+++ enums.rb
@@ -4,16 +4,17 @@ module Enumerable
       yield(item)
     end
     self
-  end
+  end 
 
   def my_each_with_index
-    result = []
-    while i < array.length
-      result 
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
   end
+end
 
-hash = Hash.new
-%w(cat dog wombat).my_each_with_index { |item, index|
-  hash[item] = index
-}
-hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+  fruits = ["apple", "banana", "strawberry", "pineapple"]
+
+  fruits.my_each_with_index { |fruit, index| puts "#{fruit} #{index}"}

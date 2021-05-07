0a373dd310d5a8092b97ac96728d05eae95ee60f
tmampa <tmampa94@gmail.com>
2021-05-07 18:18:15 +0200

    Fix linter errors

--- enums.rb
+++ enums.rb
@@ -39,6 +39,61 @@ module Enumerable
       end
     end
   end
+
+  def my_any
+    if block_given?
+      my_each { |i| return true if yield(i) }
+    else
+      my_each { |i| return true if i }
+    end
+    false
+  end
+
+  def my_none?
+    if block_given?
+      my_each { |i| return false if yield(i) }
+    else
+      my_each { |i| return false if i }
+    end
+    true
+  end
+
+  def my_count
+    if block_given?
+      counter = 0
+      my_each do |a|
+        counter += 1 if yield(a)
+      end
+    else
+      counter = length
+    end
+    counter
+  end
+
+  def my_map(arg = nil)
+    arr = []
+    i = 0
+    if !arg.nil? && arg.respond_to?(:call)
+      while i < length
+        arr << arg.call(self[i])
+        i += 1
+      end
+    elsif arg.nil? && block_given?
+      while i < length
+        arr << yield(self[i])
+        i += 1
+      end
+    end
+    arr
+  end
+
+  def my_inject(accum = self[0])
+    unshift(self[0]) unless accum == self[0]
+    (0...size).each do |i|
+      accum = yield(accum, self[i])
+    end
+    accum
+  end
 end
 
 # rubocop:enable Style/For

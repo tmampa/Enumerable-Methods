dd1b131d2ebc8220464eb6d9c591baae8f4525e2
tmampa <tmampa94@gmail.com>
2021-05-07 13:26:33 +0200

    Add my_map, my_inject methods

--- enums.rb
+++ enums.rb
@@ -21,7 +21,52 @@ module Enumerable
       i += 1
     end
   end
-end
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-numbers.my_select{|number| puts number if number % 2 == 0}
+  def my_all?
+    if block_given?
+      self.my_each do |a|
+        if yield(a)==false || yield(a).nil?
+        return false
+        end
+      end
+      true
+    else
+      self.my_all? do |b|
+      b != false && b.nil? != true
+      end
+    end
+  end
+
+  def my_count
+    if block_given?
+    counter = 0
+    self.my_each do |a|
+      counter += 1 if yield(a)
+    end
+    else
+      counter = self.length
+    end
+    counter
+  end
+
+  def my_map
+    return to_enum(:my_map) unless block_given?
+    new_arr = []
+    for i in 0...self.size
+      new_arr << yield(self[i])
+    end
+    new_arr
+  end
+
+  def my_inject(accum = self[0])
+    self.unshift(self[0]) unless accum == self[0]
+    for i in 0...self.size
+      accum = yield(accum, self[i])
+    end
+    accum
+  end
+
+  def multiply_els
+    self.my_inject { |result, element| result * element }
+  end
+end
